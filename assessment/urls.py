d2049b9e84a0027f6642da8373bd473374d034f2
faithafex22 <107470079+faithafex22@users.noreply.github.com>
2022-11-10 13:11:24 +0100

    a slight changes made to assessment.urls.py

--- assessment/urls.py
+++ assessment/urls.py
@@ -4,7 +4,7 @@ from assessment import views
 urlpatterns = [
     path('', views.AssessmentList.as_view(), name='assessment-list-create-view'),
     path('application-type/', views.ApplicationTypeList.as_view(), name='applicationtype-list-view'),
-    path('application-type/<int:id>', views.ApplicationTypeDetail.as_view(), name='applicationtype-retrieve-view'),
+    path('application-type/<int:pk>', views.ApplicationTypeDetail.as_view(), name='applicationtype-retrieve-view'),
     path('<int:pk>/', views.AssesmentDetail.as_view(), name='assessment-retrieve-update-view'),
     path('<int:pk>/categories', views.CategoryList.as_view(), name='category-list-view'),
     path('<int:assessment_id>/category/<int:id>', views.AddCategoryToAssessmentAPIView.as_view(), name="'add-category"),
