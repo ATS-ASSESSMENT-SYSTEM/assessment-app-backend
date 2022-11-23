d80fe14eadfd5ecc012b52fe0d6d9202a2f367ff
goodieboy <oyedeleyusuff@gmail.com>
2022-11-23 10:21:18 +0100

    change applicant_id to candidate on startassessmentserailizer

--- assessment/views.py
+++ assessment/views.py
@@ -69,39 +69,28 @@ class GenerateRandomQuestions(generics.CreateAPIView):
 
     def post(self, request, assessment_id, category_id):
         serializer = self.get_serializer(data=request.data)
-        print(assessment_id)
-        print(category_id)
         if serializer.is_valid():
             try:
                 assessment = Assessment.objects.get(id=assessment_id)
-                print(assessment)
                 category = Category.objects.get(id=category_id)
-                print(category)
                 current_session = AssessmentSession.objects.filter(assessment=assessment, category=category,
-                                                           candidate=serializer.data.get('applicant_id'))
+                                                           candidate=serializer.data.get('candidate'))
                 if current_session.exists():
                     questions = current_session.first().question_list.all()
                     session = current_session.first()
                 else:
                     questions = Question.objects.filter(test_category__assessment=assessment,
-                                                        test_category=category, question_categories="Real").order_by('?')[
+                                                        test_category=category, question_category="Real").order_by('?')[
                                 :category.num_of_questions]
                     session = AssessmentSession.objects.create(assessment=assessment,
-                                                               category=category,
-                                                               candidate=serializer.data['applicant_id'],
-                                                               device=serializer.data['device'],
-                                                               browser=serializer.data['browser'],
-                                                               enable_webcam=serializer.data['enable_webcam'],
-                                                               location=serializer.data['location'],
-                                                               full_screen_active=serializer.data[
-                                                                   'full_screen_active'])
+                                                               category=category, **serializer.data)
                     for question in category.question_set.all():
                         session.question_list.add(question)
             except (Assessment.DoesNotExist, Category.DoesNotExist):
                 raise ValidationError('Assessment or the category does not exist.')
             else:
                 q = GenerateQuestionSerializer(questions, many=True)
-                return Response({'session_id': session.session_id,'questions': q.data}, status=status.HTTP_200_OK)
+                return Response({'session_id': session.session_id, 'questions': q.data}, status=status.HTTP_200_OK)
         return Response({'error': serializer.errors})
 
 
