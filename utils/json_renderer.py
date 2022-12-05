7356053081518b1f5c342772af5d3bcf284e3368
goodieboy <oyedeleyusuff@gmail.com>
2022-12-05 11:14:33 +0100

    mnew changes

--- utils/json_renderer.py
+++ utils/json_renderer.py
@@ -3,6 +3,7 @@ import json
 
 from rest_framework.renderers import JSONRenderer
 from django.conf import settings as app_settings
+
 STATUS = app_settings.STATUS_CODES
 
 from Cryptodome.Cipher import AES
@@ -15,13 +16,30 @@ class CustomRenderer(JSONRenderer):
         key = b'$\x96& \xb4"\xbc,\x88\xea\xd2\xf26\xb0f\xfe'
         iv = b'%\n\xed\xa8\xdf?Tm\xd6v\xaeW9\xf4i\xb5'
         cipher = AES.new(key, AES.MODE_CBC, iv)
-        s = json.dumps(data)
+        d = {
+            "question_text": "Question 2034",
+            "question_type": "Multi-choice",
+            "question_category": "Real",
+            "choices": [
+                {
+                    "id": 63,
+                    "choice_text": "C",
+                    "is_correct": True
+                },
+                {
+                    "id": 64,
+                    "choice_text": "B",
+                    "is_correct": False
+                }
+            ]
+        }
+        s = json.dumps(d)
         encrypted_data = base64.b64encode(cipher.encrypt(pad(str.encode(s), AES.block_size))).decode('utf-8')
-        # print(encrypted_data)
+        print("data", encrypted_data)
         status_code = renderer_context["response"].status_code
         response = {
             "status": STATUS['success'],
-            "data": encrypted_data,
+            "data": data,
             "message": "Successfully Retrieved"
         }
 
