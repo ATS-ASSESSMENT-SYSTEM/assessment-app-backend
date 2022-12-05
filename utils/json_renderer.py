bd12340174be3f941c030b57bb20c6a06f38ada8
goodieboy <oyedeleyusuff@gmail.com>
2022-12-05 12:25:08 +0100

    mnew changes

--- utils/json_renderer.py
+++ utils/json_renderer.py
@@ -16,30 +16,12 @@ class CustomRenderer(JSONRenderer):
         key = b'$\x96& \xb4"\xbc,\x88\xea\xd2\xf26\xb0f\xfe'
         iv = b'%\n\xed\xa8\xdf?Tm\xd6v\xaeW9\xf4i\xb5'
         cipher = AES.new(key, AES.MODE_CBC, iv)
-        d = {
-            "question_text": "Question 2034",
-            "question_type": "Multi-choice",
-            "question_category": "Real",
-            "choices": [
-                {
-                    "id": 63,
-                    "choice_text": "C",
-                    "is_correct": True
-                },
-                {
-                    "id": 64,
-                    "choice_text": "B",
-                    "is_correct": False
-                }
-            ]
-        }
-        s = json.dumps(d)
+        s = json.dumps(data)
         encrypted_data = base64.b64encode(cipher.encrypt(pad(str.encode(s), AES.block_size))).decode('utf-8')
-        print("data", encrypted_data)
         status_code = renderer_context["response"].status_code
         response = {
             "status": STATUS['success'],
-            "data": data,
+            "data": encrypted_data,
             "message": "Successfully Retrieved"
         }
 
